7dbf2a86651ebf254ecd78fc2aca8ff324377ca8
Agus Gz <agus2012guti@gmail.com>
2023-10-27 01:31:15 -0300

    Recompongo calculadora con arrays

--- Segunda Pre entrega/main.js
+++ Segunda Pre entrega/main.js
@@ -1,40 +1,35 @@
-function sumar(a, b) {
-    return a + b;
+const operaciones = [
+  {
+    nombre: "Suma",
+    operacion: function(a, b) { return a + b; }
+  },
+  {
+    nombre: "Resta",
+    operacion: function(a, b) { return a - b; }
+  },
+  {
+    nombre: "Multiplicación",
+    operacion: function(a, b) { return a * b; }
+  },
+  {
+    nombre: "División",
+    operacion: function(a, b) { return a / b; }
   }
-  
-  function restar(a, b) {
-    return a - b;
-  }
-  
-  function multiplicar(a, b) {
-    return a * b;
-  }
-  
-  function dividir(a, b) {
-    if (b === 0) {
-      return "No es posible dividir por cero.";
-    }
-    return a / b;
-  }
-  
-  let num1 = parseFloat(prompt("Ingresa el primer número:"));
-  let num2 = parseFloat(prompt("Ingresa el segundo número:"));
-  
-  let operacion = prompt("Ingresa la operación que deseas realizar (+, -, *, /):");
-  
-  let resultado;
-  
-  if (operacion === "+") {
-    resultado = sumar(num1, num2);
-  } else if (operacion === "-") {
-    resultado = restar(num1, num2);
-  } else if (operacion === "*") {
-    resultado = multiplicar(num1, num2);
-  } else if (operacion === "/") {
-    resultado = dividir(num1, num2);
-  } else {
-    resultado = "Operación no válida";
-  }
+];
 
-  alert("Resultado: " + resultado);
-  
+const num1 = parseFloat(prompt("Ingresa el primer número:"));
+const num2 = parseFloat(prompt("Ingresa el segundo número:"));
+
+let mensajeOperaciones = "Operaciones disponibles:\n";
+for (let i = 0; i < operaciones.length; i++) {
+  mensajeOperaciones += `${i + 1}. ${operaciones[i].nombre}\n`;
+}
+
+const operacionElegida = parseInt(prompt(mensajeOperaciones)) - 1;
+
+if (operacionElegida >= 0 && operacionElegida < operaciones.length) {
+  const resultado = operaciones[operacionElegida].operacion(num1, num2);
+  alert(`Resultado de ${operaciones[operacionElegida].nombre}: ${resultado}`);
+} else {
+  alert("Operación no válida");
+}
